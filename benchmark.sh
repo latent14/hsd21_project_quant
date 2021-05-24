a3e71af6df12ab284441ce6ffac58419e03412cb
Taekmin Kim <tantara.tm@gmail.com>
2021-05-24 02:32:45 +0000

    benchmark.sh modified

--- benchmark.sh
+++ benchmark.sh
@@ -4,14 +4,14 @@
 #sudo python eval.py --num_test_images 100 --m_size 64 --v_size 64 --network mlp --run_type cpu
 #echo -e '\n=> Accuracy should be 0.97\n'
 
-# Lab7 - Conv Lowering(CPU) - Acc: 1.0
+# Lab9 - Conv Lowering(CPU) - Acc: 1.0
 sudo python eval.py --num_test_images 100 --m_size 64 --v_size 64 --network cnn --run_type cpu
 echo -e '\n=> Accuracy should be 1.0\n'
 
-# Lab12 - Quantization - Acc: 1.0
+# Quantization - Acc: 1.0
 sudo python eval.py --num_test_images 100 --m_size 64 --v_size 64 --network cnn --run_type cpu --quantized
 echo -e '\n=> Accuracy should be 1.0\n'
 
 # Project_v1
-sudo python eval.py --num_test_images 100 --m_size 64 --v_size 64 --network cnn --run_type fpga --quantized
+sudo python eval.py --num_test_images 100 --m_size 8 --v_size 8 --network cnn --run_type fpga --quantized
 echo -e '\n=> Accuracy should be 1.0\n'
